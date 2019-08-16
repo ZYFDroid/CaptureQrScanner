3dd9196b2015ee2f5e11120eb8f241bca6cc9d1b
ZYFDroid <zyfdroid@qq.com>
2019-08-16 11:20:54 +0800

    提高解码成功率

--- 截屏扫码/BarcodeResultForm.cs
+++ 截屏扫码/BarcodeResultForm.cs
@@ -25,12 +25,16 @@ namespace 截屏扫码
         {
             pictureBox1.BackgroundImage = source;
             try {
+                Bitmap optimized = new Bitmap(source, 640, 640);
+
                 BarcodeReader reader = new BarcodeReader();
                 reader.AutoRotate = true;
-                Result result= reader.Decode(source);
+                Result result= reader.Decode(optimized);
 
                 textBox1.Text = result.Text;
-            } catch { }
+            } catch (Exception ex){
+                textBox1.Text = "[解码失败]\r\n"+ex.ToString();
+            }
         }
 
         private void BarcodeResultForm_FormClosing(object sender, FormClosingEventArgs e)
